8d0683adf2dc9dd74a103ba4b25137c74f322009
osaid khan <khanosaid726@gmail.com>
2022-04-02 17:05:58 +0530

    project added

---
+++ src/test/java/com/spring/webapp/quick/QuickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.webapp.quick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
